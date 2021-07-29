f77065c1f19743d298f5a057fae86c112668401a
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-07-30 00:06:11 +0530

    grid

---
+++ jsgame/Jeopardy/index.js
@@ -0,0 +1,35 @@
+/* 
+    Jeopardy
+    
+    Write an async function 
+        that uses fetch() to fetch 4 categories 
+        from https://jservice.io/api/categories
+        parameters: count, offset
+        
+    Display the categories
+        in a simplified 4x5 Jeopardy Board 
+        using CSS Grid
+*/
+
+async function getCategories() {
+    let response = await fetch("https://jservice.io/api/categories?count=4&offset=30")
+    let data = await response.json()
+    return data
+}
+
+function getCategoryHtml(category) {
+    return `
+        <div class="my-category-title">${category.title}</div>
+        <div class="my-category-clue" style="grid-row-start: 2">$100</div>
+        <div class="my-category-clue" style="grid-row-start: 3">$200</div>
+        <div class="my-category-clue" style="grid-row-start: 4">$300</div>
+        <div class="my-category-clue" style="grid-row-start: 5">$400</div>
+    `
+}
+
+getCategories().then(categories => {
+    console.log(categories)
+    document.body.innerHTML = `<div class="board">
+        ${categories.map(getCategoryHtml).join('')}
+    </div>`
+})
