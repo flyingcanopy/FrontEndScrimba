98acfe5ac0e40435037d3811b5b0c397afb2607a
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-07-30 19:56:46 +0530

    props passing objects

--- React/ReactDom and JSx/reactdom-jsx/src/component/App.js
+++ React/ReactDom and JSx/reactdom-jsx/src/component/App.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from "react"
+import ContactCard from "./ContactCard"
 
-import Navbar from './Navbar'
-import MainContent from './MainContent'
-import Footer  from './footer'
-  // 1. A Navbar component
-  // 2. A MainContent component
-  // 3. A Footer component
-
-
-function App(){
-  return (
-    <div>
-      <Navbar/>
-      <MainContent/>
-      <Footer/>
-    </div>
-  )
+function App() {
+    return (
+        <div className="contacts">
+            <ContactCard 
+                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+            
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
+            
+        </div>
+    )
 }
+
 export default App
