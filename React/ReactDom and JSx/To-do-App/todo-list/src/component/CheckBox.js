e3d86c2fae0ddf8c223da81acbb245b952eb8b04
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-07-30 18:05:50 +0530

    insert jsscript into jsx

--- React/ReactDom and JSx/To-do-App/todo-list/src/component/CheckBox.js
+++ React/ReactDom and JSx/To-do-App/todo-list/src/component/CheckBox.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 function CheckBox() {
-  return (
+    let arr = ['laundry','chores','work']
+    return (
     <form className="form-box" >
       <input type="checkbox" id="vehicle1" name="vehicle1" value="laundry" />
-      <label for="vehicle1"> Do the laundry</label>
+      <label for="vehicle1"> Do the {arr[0]}</label>
       <br />
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="chores" />
-      <label for="vehicle2"> Do the chores</label>
+      <input type="checkbox" id="vehicle2" name="vehicle2" value="work" />
+      <label for="vehicle2"> Do the {arr[1]}</label>
       <br />
-      <input type="checkbox" id="vehicle3" name="vehicle3" value="work" />
-      <label for="vehicle3"> Do the work</label>
+      <input type="checkbox" id="vehicle3" name="vehicle3" value="chores" />
+      <label for="vehicle3"> Do the {arr[2]}</label>
       <br />
     </form>
   );
