2cec5f412c8e4f3b9e249e31873fbcf70f727f99
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-07-31 21:46:15 +0530

    todolist state

--- React/ReactDom and JSx/To-do-App/todo-list/src/component/ToDoItem.js
+++ React/ReactDom and JSx/To-do-App/todo-list/src/component/ToDoItem.js
@@ -1,48 +1,21 @@
-import React from "react";
-// {
-//   id: 1,
-//   text: "Take out the trash",
-//   completed: true
-// }
-// function ToDoItem(props) {
-//     let todoItem = props.todoItem
-//     console.log(todoItem)
-//     return (
-//         <div className="todo-item">
-//             <input type="checkbox" checked={todoItem.completed}/>
-//             <p>{todoItem.text}</p>
-//         </div>
-//     )
-// }
+import React from "react"
 
-class TodoItem extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-  render() {
-    let todoItem = this.props.todoItem;
-    this.state = {
-      todoItem: this.props.todoItem,
-    };
-    let count=0
-    setInterval(() => {
-        
-      this.state.todoItem = {
-        text: "ZZZ"+count++,
-        completed: true,
-      }
-      console.log(this.state.todoItem)
-    }, 1000);
-
-    console.log(todoItem);
+function TodoItem(props) {
+  
+    console.log(props.method)
+    
     return (
-      <div className="todo-item">
-        <input type="checkbox" checked={this.state.todoItem.completed} />
-        <p>{this.state.todoItem.text}</p>
-      </div>
-    );
-  }
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={()=>{
+                    props.method(props.item.id)
+                }}
+            />
+            <p>{props.item.text}</p>
+        </div>
+    )
 }
 
-export default TodoItem;
+export default TodoItem
