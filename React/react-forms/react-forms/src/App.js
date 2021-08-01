026c5f7f5cf3a376b4438b3dc3b3e26af2d9ccf0
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-08-01 15:51:08 +0530

    react-forms-controlling

---
+++ React/react-forms/react-forms/src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(event) {
+        const {name, value} = event.target
+      
+        this.setState({
+            [name]: value
+        })
+        console.log(this.state)
+    }
+    
+    render() {
+        return (
+          //Controlled forms in react.. The value in rendered is controlled by the state .. value={this.state.firstName} 
+            <form>
+                <input 
+                    type="text" 
+                    value={this.state.firstName} 
+                    name="firstName" 
+                    placeholder="First Name" 
+                    onChange={this.handleChange} 
+                />
+                <br />
+                <input 
+                    type="text" 
+                    value={this.state.lastName} 
+                    name="lastName" 
+                    placeholder="Last Name" 
+                    onChange={this.handleChange} 
+                />
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
+        )
+    }
+}
+
+export default App
