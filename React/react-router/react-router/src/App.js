d49f85fce0f968c0a5a8d3a8f463d97eb592259f
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-09-10 15:36:22 +0530

    react-router

--- React/react-router/react-router/src/App.js
+++ React/react-router/react-router/src/App.js
@@ -1,52 +1,39 @@
-import React from "react";
-import { Link, Switch, BrowserRouter as Router } from "react-router-dom";
-function About() {
-  return (
-    <div>
-      <h1>Hey im back</h1>
-    </div>
-  );
-}
-function App() {
-  const styleObj = {
-    display: "flex",
-    "flex-direction": "row",
-    "justify-content": "space-between",
-  };
-  return (
-    <div>
-      <nav>
-        <ul style={styleObj}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Router path="/contact">
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import Profile from "./pages/profile/Profile"
+import Info from "./pages/profile/Info"
+import Settings from "./pages/profile/Settings"
+import {Switch, BrowserRouter as Router} from "react-router-dom"
+
+function App() {    
+    return (
         <div>
-            <h1> Contact</h1>
-          </div>
-        </Router>
-        <Router path="/About">
-          <div>
-            <h1> About</h1>
-          </div>
-        </Router>
-        <Router path="/">
-         <div>
-            <h1> Home </h1>
-          </div>
-        </Router>
-      </Switch>
-    </div>
-  );
+            <Header />
+            
+            <Switch>
+                <Router exact path="/">
+                    <Home />
+                </Router>
+               
+                <Router path="/profile/info">
+                <Info/>
+
+                </Router>
+                <Router path="/profile/settings">
+                 
+                <Settings/>
+                </Router>
+                <Router exact path="/profile">
+                    <Profile/>
+                </Router>
+            </Switch>
+           
+            
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ React/react-router/react-router/src/pages/profile/Profile.js
@@ -0,0 +1,18 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+
+function Profile() {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <ul>
+                <li><Link to="/profile/info">Profile Info</Link></li>
+                <li><Link to="/profile/settings">Profile Settings</Link></li>
+            </ul>
+       
+        </div>
+    )
+}
+
+export default Profile
