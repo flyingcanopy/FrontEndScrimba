04ace45c3f283787a011cbb8d28379d1ae66f1de
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-09-12 00:29:48 +0530

    total items added

--- React/capestone-shopping-cart/shopping-cart/src/pages/Cart.js
+++ React/capestone-shopping-cart/shopping-cart/src/pages/Cart.js
@@ -3,15 +3,18 @@ import { useContext } from "react"
 import { Context } from "../components/ContextProvider"
 import CartItem from "../components/CartItem"
 function Cart() {
+    
     const {cartItems} = useContext(Context)
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
     ))
+    const price = 5.99
+    const totalPrice = 5.99 * (cartItemElements.length)
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total:{totalPrice.toLocaleString("en-US", {style: "currency", currency: "INR"})} </p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
