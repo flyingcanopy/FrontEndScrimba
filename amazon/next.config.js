dab5d3873b15a4dcf203f803aaa47995c49e441a
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-09-17 19:21:51 +0530

    fixed next.config.js

--- amazon/next.config.js
+++ amazon/next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+	 webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+	  config.resolve.fallback.net = false;
+	  config.resolve.fallback.dns = false;
+	  config.resolve.fallback.child_process = false;
+	  config.resolve.fallback.tls = false;
+	  config.resolve.fallback.dgram = false;
+	  config.resolve.fallback.module = false;	  
+    }
+    return config;
+  },
     images : {
         domains:['links.papareact.com','www.freepnglogos.com','fakestoreapi.com']
     }
